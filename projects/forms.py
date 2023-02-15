cbb04dde0e67c6f7c5c8b2cdf8ce1018e6ee2ca0
Ayon Sen <ayon.s2000@gmail.com>
2023-02-15 22:32:58 +0530

    Theme Installation

--- projects/forms.py
+++ projects/forms.py
@@ -5,6 +5,9 @@
 from django.forms import ModelForm
 from .models import Project
 
+from django import forms
+
+
 #* Model form defined as class
 class ProjectForm(ModelForm):
     class Meta:
@@ -13,3 +16,18 @@ class ProjectForm(ModelForm):
         model = Project
         #* Fields from model to be included in form
         fields = ['title','featured_image','description','demo_link','source_link','tags']
+
+        #* Tags now displayed as checkboxes
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self,*args,**kwargs) :
+        super(ProjectForm,self).__init__(*args,**kwargs)
+
+            # self.fields['title'].widget.attrs.update(
+            #     {'class':'input','placeholder':'Add Title'})
+
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+            
